c3aad0da9d9a24c782fca0ff5cb77e1e3f64924f
sarojini <124454596+sarojinimandapati@users.noreply.github.com>
2025-10-26 16:29:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+import os
 
 app=Flask(__name__)
 
 @app.route("/")
 def index():
-    return "This is my first git upload"
+    return "This is my first render upload"
 
 
 if __name__=="__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's port
+    app.run(host="0.0.0.0", port=port)
